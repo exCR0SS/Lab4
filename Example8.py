993e6633ee55172b2b90f7987951381197fffd67
exCR0SS <MailboxofficialPetrovAA@gmail.com>
2023-01-06 14:45:00 +0500

    Шифр цезаря, работает пока с 1 словом

---
+++ Example8.py
@@ -0,0 +1,45 @@
+# Напишите программу «Шифр Цезаря», которая зашифровывает
+# введенный текст. Используете кодовую таблицу символов. При запуске
+# программы в консоль необходимо вывести сообщение: «Введите текст для
+# шифрования», после ввода текста, появляется сообщение: «Введите ключ».
+# После того как введены все данные, необходимо вывести преобразованную
+# строку с сообщением «Текст после преобразования : ». Далее необходимо
+# задать вопрос пользователю: «Выполнить обратное преобразование? (y/n)»,
+# если пользователь вводит «y», тогда выполнить обратное преобразование.
+# Если пользователь вводит «n», того программа выводит сообщение «До
+# свидания!». Если пользователь вводит что-то другое, отличное от «y» или «n»,
+# то программа ему выводит сообщение: «Введите корректный ответ».
+
+
+class Main:
+
+    userText = str(input("Введите слово для шифрования: ").lower())
+    userKey = int(input("Введите ключ: "))
+
+
+    def get_encrypt(txt, key, encrypt):
+        alphabetEN = 'abcdefghijklmnopqrstuvwxyz'
+        alphabetRU = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+        resText = ''
+
+        if alphabetRU.find(txt[0]) != -1:
+            for i in range(len(txt)):
+                resText += alphabetRU[alphabetRU.index(txt[i]) + (key if encrypt == True else -abs(key))]
+        else:
+            for i in range(len(txt)):
+                resText += alphabetEN[alphabetEN.index(txt[i]) + (key if encrypt == True else -abs(key))]
+        return resText
+
+
+    print(get_encrypt(userText, userKey, True))
+
+    while True:
+        decisionUser = str(input("Выполнить обратное преобразование? (y/n) ").lower())
+        if decisionUser == 'y':
+            print(get_encrypt(get_encrypt(userText, userKey, True), userKey, False))
+            break
+        elif decisionUser == 'n':
+            break
+        else:
+            print("Введите корректный ответ.")
+            continue
